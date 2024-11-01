3a6670abec32f1066919bbcc1eefa9c8ead5a14f
eMD0gG <147574275+eMD0gG@users.noreply.github.com>
2024-11-02 01:10:07 +1000

    templates update

--- csgorun/urls.py
+++ csgorun/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='main_page'),
     path('cases/<str:name>/', views.case, name='case_page'),
     path('case_api/<str:name>/', views.case_api, name='case_page'),
-]
 
+    path('logout/', views.custom_logout, name='logout'),
+]
--- csgorun/views.py
+++ csgorun/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.http import HttpRequest
 from .models import *
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -19,11 +21,16 @@ def case(request: HttpRequest, name: str) -> HttpResponse:
 
     skins.sort(key=lambda skin: skin.cost, reverse=True)
 
-    return render(request, f'csgorun/case_{name}.html', {"skins": skins})
+    return render(request, f'csgorun/case_page.html', {"skins": skins, "case" : case_})
 
 def case_api(request: HttpRequest, name: str) -> JsonResponse:
     case_ = Case.objects.get(name=name)
     print(case_.pull)
 
     return JsonResponse(case_.pull, safe=False)
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('/')
 
