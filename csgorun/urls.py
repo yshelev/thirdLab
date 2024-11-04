31a041c809a221f9a594028cdb87aaf030b23faf
zxcpisya <mamin.papa0@gmail.com>
2024-11-04 18:40:58 +1000

    payment system is working

--- csgorun/urls.py
+++ csgorun/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'csgorun'
+
 urlpatterns = [
     path('', views.index, name='main_page'),
     path('cases/<str:name>/', views.case, name='case_page'),
--- thirdLab/urls.py
+++ thirdLab/urls.py
@@ -5,6 +5,7 @@ from csgorun import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('social_django.urls', namespace='social')),
-    path('', include('csgorun.urls')),
+    path('', include('social_django.urls')),
+    path('', include('csgorun.urls', namespace='csgorun')),
+    path('', include('yoomoney.urls', namespace='yoomoney')),
 ]
