91b318d0fe41aebb88a8519a5dbef33ef5543e74
zxcpisya <mamin.papa0@gmail.com>
2024-11-10 01:06:07 +1000

    add effect to skin-chose

--- static/Инвентарь_files/chose_skin.js
+++ static/Инвентарь_files/chose_skin.js
@@ -1,15 +1,16 @@
 function on_skin_click(element, amount) {
     amount = Number(amount)
     chosen_skins_cost = document.getElementById("chosen_skins_cost")
-    if (element.classList.contains("chosen_item")) {
-        chosen_skins_cost.innerHTML = (Number(chosen_skins_cost.innerHTML.slice(0, -1)) + amount).toString() + "₽" + `<div className="text-nano font-medium text-[#a6adcd]">Выбрано скинов в
-                инвентаре
-            </div>`
+    if (!element.classList.contains("chosen_item")) {
+        console.log(chosen_skins_cost.innerHTML.split("₽")[0])
+        chosen_skins_cost.innerHTML = (Number(chosen_skins_cost.innerHTML.split("₽")[0]) + amount).toString() + `₽<div class="text-nano font-medium text-[#a6adcd]">Выбрано скинов в
+                                        инвентаре
+                                    </div>`
         element.classList.add("chosen_item")
     } else {
-        chosen_skins_cost.innerHTML = (Number(chosen_skins_cost.innerHTML.slice(0, -1)) - amount).toString() + "₽" + `<div className="text-nano font-medium text-[#a6adcd]">Выбрано скинов в
-                инвентаре
-            </div>`
-        element.classList.add("chosen_item")
+        chosen_skins_cost.innerHTML = (Number(chosen_skins_cost.innerHTML.split("₽")[0]) - amount).toString() + `₽<div class="text-nano font-medium text-[#a6adcd]">Выбрано скинов в
+                                        инвентаре
+                                    </div>`
+        element.classList.remove("chosen_item")
     }
 }
